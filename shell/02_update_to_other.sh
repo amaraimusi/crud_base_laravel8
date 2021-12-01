836e9cac4023031a90ff2ef5647bba51524fe50e
user <user@DESKTOP-PE1E9IO>
2021-12-01 23:23:37 +0900

    shellを更新

---
+++ shell/02_update_to_other.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/sh
+echo 'CrudBaseを他のプロジェクトへ更新'
+
+rsync -auvz ~/git/crud_base_laravel8/dev/public/css/CrudBase ~/git/cake_demo/app/webroot/css/
+rsync -auvz ~/git/crud_base_laravel8/dev/public/js/CrudBase ~/git/cake_demo/app/webroot/js/
+rsync -auvz ~/git/crud_base_laravel8/dev/vendor/CrudBase ~/git/cake_demo/app/Vendor/
+
+
+echo "------------ Success!"
+cmd /k
