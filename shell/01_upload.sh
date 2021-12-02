8b14f4f6fe7ddfbc7a5094a4869ae6d09d621764
user <user@DESKTOP-PE1E9IO>
2021-12-03 08:54:35 +0900

    本番環境へのアップロードシェルを修正

--- shell/01_upload.sh
+++ shell/01_upload.sh
@@ -1,7 +1,13 @@
 ﻿#!/bin/sh
 echo 'ソースコードを差分アップロードします。'
 
-rsync -auvz --exclude='.env' ../dev amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/
+rsync -auvz ../dev/app amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
+rsync -auvz ../dev/bootstrap amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
+rsync -auvz ../dev/config amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
+rsync -auvz ../dev/public amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
+rsync -auvz ../dev/resources amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
+rsync -auvz ../dev/routes amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
+rsync -auvz ../dev/crud_base_config.php amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
 
 echo "------------ Success"
 #cmd /k
