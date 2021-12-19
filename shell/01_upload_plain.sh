55136bc5713f69e04525bc100c4776fe1d6f1f73
user <user@DESKTOP-PE1E9IO.mshome.net>
2021-12-20 07:19:37 +0900

    shell plain関係を修正

--- shell/01_upload_plain.sh
+++ shell/01_upload_plain.sh
@@ -1,7 +1,9 @@
 ﻿#!/bin/sh
 echo 'ソースコードを差分アップロードします。'
 
-rsync -auvz ../plain amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/
+rsync -auvz --exclude='crud_base_config.php' ../plain amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/
+rsync -auvz ../plain/app/crud_base_config_p.php amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/plain/app/crud_base_config.php
+
 
 echo "------------ Success"
-#cmd /k
+cmd /k
--- shell/02_u_cb_config_plain.sh
+++
@@ -1,7 +0,0 @@
-﻿#!/bin/sh
-echo 'ソースコードを差分アップロードします。'
-
-rsync -auvz ../plain/crud_base_config_p.php amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/plain/crud_base_config.php
-
-echo "------------ Success"
-cmd /k
