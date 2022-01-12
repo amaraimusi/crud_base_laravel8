90e270d3304eba2f95384d9331776d4d23e96307
user <user@host.docker.internal>
2022-01-12 15:59:48 +0900

    アップロードシェルの修正 2

--- shell/01_upload.sh
+++ shell/01_upload.sh
@@ -7,6 +7,7 @@ rsync -auvz ../dev/config amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_larav
 rsync -auvz ../dev/public amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
 rsync -auvz ../dev/resources amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
 rsync -auvz ../dev/routes amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/
+rsync -auvz ../dev/vendor/CrudBase amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/vendor
 rsync -auvz ../dev/crud_base_config_p.php amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/crud_base_config.php
 rsync -auvz ../dev/.env_p amaraimusi@amaraimusi.sakura.ne.jp:www/crud_base_laravel8/dev/.env
 
