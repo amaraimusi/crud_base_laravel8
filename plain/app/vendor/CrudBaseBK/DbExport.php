2514daa1de0714d15cce4370058f17133a2b3d85
user <user@host.docker.internal>
2022-06-04 12:53:47 +0900

    CrudBaseライブラリはdev側で一元管理することにしたのでplain側は除去する。

--- plain/app/vendor/CrudBaseBK/DbExport.php
+++
@@ -1,7 +0,0 @@
-<?php
-class DbExport{
-	
-	public function test(IDao $dao){
-		$data = $dao->sqlExe('SELECT * FROM nekos WHERE id=62');
-	}
-}
--- plain/app/vendor/CrudBaseBK/IDao.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface IDao{
-	public function sqlExe($sql);
-	public function begin();
-	public function rollback();
-	public function commit();
-}
